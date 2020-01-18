d7d1b01a3d7cfceeed0c6964dc21ce9270ae09bc
Duvan González <duvan4448@gmail.com>
2020-01-17 22:50:44 -0500

    2-Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    NewComponentComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
