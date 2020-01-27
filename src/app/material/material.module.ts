0c523fedb04c7b89be5a3afd069a0deeabc37446
Duvan González <duvan4448@gmail.com>
2020-01-26 20:25:41 -0500

    11-Lista-inventario-detalle

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -9,6 +10,16 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatTableModule } from '@angular/material/table';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSortModule } from '@angular/material/sort';
+
+
 
 @NgModule({
   declarations: [],
@@ -22,6 +33,13 @@ import { MatRadioModule } from '@angular/material/radio';
     MatInputModule,
     MatSelectModule,
     MatRadioModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatSortModule
   ],
   exports: [
     MatButtonModule,
@@ -32,6 +50,13 @@ import { MatRadioModule } from '@angular/material/radio';
     MatInputModule,
     MatSelectModule,
     MatRadioModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatSortModule
   ]
 })
 export class MaterialModule { }
