2b9e768bc9c46aa7d548fad877cd4a22317a8c9e
Duvan González <duvan4448@gmail.com>
2020-02-02 11:27:11 -0500

    17-ProgramacionReactiva-Forma2-Pipes

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { map } from 'rxjs/operators';
+
 import { CartService } from './../../../core/services/cart.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,16 +12,16 @@ import { CartService } from './../../../core/services/cart.service';
 })
 export class HeaderComponent implements OnInit {
 
-  total = 0;
+  total$: Observable<number>;
 
   constructor(
     /*A esto se le llama inyección de dependencias*/
     private cartService: CartService
   ) {
-    this.cartService.cart$.subscribe(products => {
-      console.log(products);
-      this.total = products.length;
-    });
+    this.total$ = this.cartService.cart$
+      .pipe(
+        map(products => products.length)
+      );
   }
 
   ngOnInit() {
